7ab5bca6527f7f39b98633f03f1949ad34c497eb
Yaguarete4 <111437151+Yaguarete4@users.noreply.github.com>
2022-10-20 14:04:52 -0300

    Coliciones

--- Bill-E Proyecto 2022/Bill-E Proyecto 2022/Form8.Designer.cs
+++ Bill-E Proyecto 2022/Bill-E Proyecto 2022/Form8.Designer.cs
@@ -28,8 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form8));
             this.btnVoler2 = new System.Windows.Forms.Button();
+            this.moveTimer = new System.Windows.Forms.Timer(this.components);
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.Red = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Red)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // btnVoler2
@@ -50,6 +62,71 @@
             this.btnVoler2.MouseEnter += new System.EventHandler(this.BtnVolver_MouseEnter);
             this.btnVoler2.MouseLeave += new System.EventHandler(this.BtnVolver_MouseLeave);
             // 
+            // moveTimer
+            // 
+            this.moveTimer.Enabled = true;
+            this.moveTimer.Interval = 1;
+            this.moveTimer.Tick += new System.EventHandler(this.EventoMoveTimer);
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.Transparent;
+            this.Player.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Player.BackgroundImage")));
+            this.Player.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Player.Location = new System.Drawing.Point(112, 131);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(60, 60);
+            this.Player.TabIndex = 7;
+            this.Player.TabStop = false;
+            // 
+            // Red
+            // 
+            this.Red.BackColor = System.Drawing.Color.Transparent;
+            this.Red.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Red.BackgroundImage")));
+            this.Red.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Red.Location = new System.Drawing.Point(235, 237);
+            this.Red.Name = "Red";
+            this.Red.Size = new System.Drawing.Size(35, 35);
+            this.Red.TabIndex = 8;
+            this.Red.TabStop = false;
+            this.Red.Tag = "redBall";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(383, 208);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "redBall";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(511, 131);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "redBall";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(297, 71);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "redBall";
+            // 
             // Form8
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -57,10 +134,23 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Red);
+            this.Controls.Add(this.Player);
             this.Controls.Add(this.btnVoler2);
             this.DoubleBuffered = true;
+            this.KeyPreview = true;
             this.Name = "Form8";
             this.Text = "Form8";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form8_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form8_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Red)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -68,5 +158,11 @@
         #endregion
 
         private System.Windows.Forms.Button btnVoler2;
+        private System.Windows.Forms.Timer moveTimer;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.PictureBox Red;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
--- Bill-E Proyecto 2022/Bill-E Proyecto 2022/Form8.cs
+++ Bill-E Proyecto 2022/Bill-E Proyecto 2022/Form8.cs
@@ -13,6 +13,8 @@ namespace Bill_E_Proyecto_2022
     public partial class Form8 : Form
     {
         GeneralButton volver;
+        bool moveRight, moveLeft, moveUp, moveDown;
+        int velocidad = 3;
         public Form8()
         {
             InitializeComponent();
@@ -34,6 +36,91 @@ namespace Bill_E_Proyecto_2022
         private void BtnVolver_MouseLeave(object sender, EventArgs e)
         {
             volver.setDefault();
+        }
+
+        private void Form8_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                moveLeft = true;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                moveRight = true;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                moveUp = true;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                moveDown = true;
+            }
+        }
+
+        private void Form8_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                moveLeft = false;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                moveRight = false;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                moveUp = false;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                moveDown = false;
+            }
+        }
+
+        private void EventoMoveTimer(object sender, EventArgs e)
+        {
+            //Hitbox Programe
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "redBall")
+                {
+                    //Makes the player unable to move to a certain direction inside the cube.
+//                  | Defines the left perimeter |       Defines the top perimeter       |    Defines the down perimeter    | Defines the Right Perimeter |
+                    if((x.Left - Player.Right) < 1  && (x.Top - Player.Top) < (Player.Height - 2) && (Player.Top - x.Top) < (x.Height -2) && (Player.Left + 2) < x.Right)
+                    {
+                        moveRight = false;
+                    }
+                    if ((Player.Left - x.Right) < 1 && (x.Top - Player.Top) < (Player.Height - 2) && (Player.Top - x.Top) < (x.Height - 2) && (x.Left + 2) < Player.Right)
+                    {
+                        moveLeft = false;
+                    }
+                    if((x.Top - Player.Top) < (Player.Height + 2) && (x.Left + 2) < Player.Right && (Player.Left + 2) < x.Right && (Player.Top - x.Top) < (x.Height - 2)){
+                        moveDown = false;
+                    }
+                    if((Player.Top - x.Top) < (x.Height + 2) && (x.Left + 2) < Player.Right && (Player.Left + 2) < x.Right && (x.Top - Player.Top) < (Player.Height - 2))
+                    {
+                     moveUp = false;
+                    }
+                }
+            }
+
+            if (moveLeft == true && Player.Left > 0)
+            {
+                Player.Left -= velocidad;
+            }
+            if (moveRight == true && Player.Left < 739)
+            {
+                Player.Left += velocidad;
+            }
+            if (moveUp == true && Player.Top > 0)
+            {
+                Player.Top -= velocidad;
+            }
+            if (moveDown == true && Player.Top < 353)
+            {
+                Player.Top += velocidad;
+            }
         }
     }
 }
